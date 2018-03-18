8bb2cf802788521476908968610817f67e47c7b8
Lorenz Schymanietz <yungcoder@protonmail.com>
2018-03-18 21:17:46 +0000

    Update str.c

--- examples/str.c
+++ examples/str.c
@@ -7,7 +7,7 @@ int main() {
     
     string test = MA_GET_ARRAY(string, "Hello, World!");
 
-    // test.len = strlen(test.array); //small fix in case of char* variable
+    // test.len = strlen(test.array); //small fix in case of char* variable, requires <string.h>
 
     for (unsigned i = 0; i < test.len; i++) {
         printf("%c", ma_get_value_string(&test, i));
