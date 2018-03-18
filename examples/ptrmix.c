84ba390fb3069b633092110533ae7a6b25ed3818
Lorenz Schymanietz <yungcoder@protonmail.com>
2018-03-18 20:42:07 +0000

    Create ptrmix.c
    Small attempt at using this with a 2d float array.

---
+++ examples/ptrmix.c
@@ -0,0 +1,36 @@
+
+#include "ma_better.h"
+
+DEFINE_BETTER_STRUCT(float*, floatptr_array);
+
+int main() {
+
+    float* array_normal[2] = {
+        malloc(sizeof(float) * 3), 
+        malloc(sizeof(float) * 3)
+    };
+    
+    for (unsigned i = 1; i <= 2; i++) {
+        for (unsigned n = 1; n <= 3; n++) {
+            array_normal[i-1][n-1] = n + i;
+        }
+    }
+
+    floatptr_array test = MA_GET_ARRAY(
+        floatptr_array, 
+        array_normal
+    );
+
+    for (unsigned i = 0; i < test.len; i++) {
+        for (unsigned n = 0; n < 3; n++) {
+            printf("#%.1f ", ma_get_value_floatptr_array(&test, i)[n]);
+        }
+    }
+
+    printf("\n");
+
+    free(array_normal[0]);
+    free(array_normal[1]);
+
+    return 0;
+}
