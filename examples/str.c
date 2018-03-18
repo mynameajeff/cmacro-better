2ddb11e6ae7c706ed7b64ed938cf83808c542d04
Lorenz Schymanietz <yungcoder@protonmail.com>
2018-03-18 21:04:08 +0000

    Update str.c
    Added show of `ma_reverse_arr`, and made some small adjustments.

--- examples/str.c
+++ examples/str.c
@@ -1,23 +1,30 @@
 
-#include "../ma_better.h"
+#include "ma_better.h"
 
 DEFINE_BETTER_STRUCT(char, string);
 
 int main() {
-
-    char* array_normal = "Hello, World!";
     
     string test = MA_GET_ARRAY(
         string, 
-        array_normal
+        "Hello, World!"
     );
 
-    test.len = strlen(test.array); //small fix, as since this is with a pointer, it can't detect the length correctly.
+    // test.len = strlen(test.array); //small fix in case of char* variable
 
     for (unsigned i = 0; i < test.len; i++) {
         printf("%c", ma_get_value_string(&test, i));
     }
     printf("\n");
+
+    test = ma_reverse_arr_string(&test);
+
+    for (unsigned i = 0; i < test.len; i++) {
+        printf("%c", ma_get_value_string(&test, i));
+    }
+    printf("\n");
+
+    ma_clean_arr_string(&test);
 
     return 0;
 }
