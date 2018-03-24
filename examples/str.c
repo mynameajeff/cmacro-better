d034129ff2ba77a9e0b11271356017f9c97625f7
Lorenz Schymanietz <yungcoder@protonmail.com>
2018-03-24 09:28:54 +0000

    Update str.c

--- examples/str.c
+++ examples/str.c
@@ -3,25 +3,43 @@
 
 MA_DEFINE_BETTER_STRUCT(char, string);
 
+void show_string(string* str);
+
 int main() {
 
-    string test = MA_GET_ARRAY(string, "Hello, World!");
+    char str_array[] = "Hello, World!";
+
+    string test = MA_GET_ARRAY(string, str_array);
 
     // test.len = strlen(test.array); //small fix in case of char* variable, requires <string.h>
 
-    for (unsigned i = 0; i < test.len; i++) {
-        printf("%c", ma_get_value_string(&test, i));
-    }
-    printf("\n");
+    show_string(&test);
 
     test = ma_reverse_string(&test);
 
-    for (unsigned i = 0; i < test.len; i++) {
-        printf("%c", ma_get_value_string(&test, i));
-    }
-    printf("\n");
+    //remove null terminator from beginning of str, ma_reverse_string fucks with ma_swap_values_string
+    test = ma_del_value_string(&test, 0);
+
+    show_string(&test);
+
+    ma_swap_values_string(&test, 0, 1);
+
+    show_string(&test);
+
+    test = ma_reverse_string(&test);
+
+    show_string(&test);
 
     ma_clean_string(&test);
 
     return 0;
+}
+
+void show_string(string* str) {
+
+    for (unsigned i = 0; i < str->len; i++) {
+        printf("%c", ma_get_value_string(str, i));
+    }
+
+    printf("\n");
 }
